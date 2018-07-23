50871f271d26dc77600dff45b5af7d7d48ba0c41
Neil Wan <neilw@synnex.com>
2018-07-23 23:22:08 +0800

    update dbutil

--- src/main/java/com/nupday/util/DBUtil.java
+++ src/main/java/com/nupday/util/DBUtil.java
@@ -1,4 +1,11 @@
 package com.nupday.util;
+
+import com.nupday.config.DBInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -7,16 +14,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import com.nupday.config.DBInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
-
 @Service
 public class DBUtil {
 
     private final static Logger logger = LoggerFactory.getLogger(DBUtil.class);
+
     /**
      * 备份数据库操作
      *
@@ -32,40 +34,65 @@ public class DBUtil {
         //构建远程数据库命令字符串  
         StringBuilder cmdStr = new StringBuilder();
         cmdStr.append(" -u")
-              .append(dbConfig.getUserName())
-              .append(" -p")
-              .append(dbConfig.getPassword())
-              .append(" -h")
-              .append(dbConfig.getHost())
-              .append(" -P")
-              .append(dbConfig.getPort())
-              .append(" ").append(dbConfig.getDatabase());
+                .append(dbConfig.getUserName())
+                .append(" -p")
+                .append(dbConfig.getPassword())
+                .append(" -h")
+                .append(dbConfig.getHost())
+                .append(" -P")
+                .append(dbConfig.getPort())
+                .append(" ").append(dbConfig.getDatabase());
 
         Runtime rt = Runtime.getRuntime();
         // 调用mysqldump的cmd命令  
         final Process p;
         if (isWindows()) {
-            p = rt.exec(new String[] {"cmd", "/c", f.getPath() + cmdStr.toString()});
+            p = rt.exec(new String[]{"cmd", "/c", f.getPath() + cmdStr.toString()});
         } else {
-            p = rt.exec(new String[] {"sh", "-c", "cd " + f.getParent() + " | mysqldump " + cmdStr.toString()});
+            p = rt.exec(new String[]{"sh", "-c", "cd " + f.getParent() + " | mysqldump " + cmdStr.toString()});
         }
 
         //得到远程数据库备份的流文件
         InputStream in = p.getInputStream();
         StringBuilder stringBuilder = new StringBuilder();
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String line1 = null;
-        Integer count = 0;
-        while ((line1 = reader.readLine()) != null) {
-            if (line1 != null) {
-                count ++;
-                stringBuilder.append(line1).append("\r\n");
-            } else {
-                break;
+        new Thread(() -> {
+            String line1;
+            Integer count = 0;
+            try {
+                while ((line1 = reader.readLine()) != null) {
+                    if (line1 != null) {
+                        count++;
+                        stringBuilder.append(line1).append("\r\n");
+                    } else {
+                        break;
+                    }
+                }
+            } catch (Exception e) {
             }
-        }
-        logger.info(stringBuilder.toString());
-        logger.info(count + "");
+            logger.info(stringBuilder.toString());
+            logger.info(count + "");
+        }).start();
+        InputStream is2 = p.getErrorStream();
+        new Thread(() -> {
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(is2));
+            try {
+                String line2;
+                while ((line2 = br2.readLine()) != null) {
+                    if (line2 != null) {
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is2.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        p.waitFor();
         return new ByteArrayInputStream(stringBuilder.toString().getBytes());
     }
 
