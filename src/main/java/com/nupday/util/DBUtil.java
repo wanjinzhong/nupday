a87d737e7ac238c0bca5f63c39527298e3b70212
Neil Wan <neilw@synnex.com>
2018-07-24 00:16:45 +0800

    update dbutil

--- src/main/java/com/nupday/util/DBUtil.java
+++ src/main/java/com/nupday/util/DBUtil.java
@@ -10,14 +10,17 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Date;
 
 @Service
 public class DBUtil {
 
-    private final static Logger logger = LoggerFactory.getLogger(DBUtil.class);
 
     /**
      * 备份数据库操作
@@ -25,13 +28,24 @@ public class DBUtil {
      * @return BackupInfo备份状态对象
      * @throws IOException 文件读写错误
      */
-    public static InputStream backup(DBInfo dbConfig) throws IOException, InterruptedException {
-        File f = getMysqlDump();
-        if (f == null) {
+    public static File backup(DBInfo dbConfig) throws IOException, InterruptedException {
+        File mysqlFile = getMysqlDump();
+        if (mysqlFile == null) {
             throw new FileNotFoundException("在lib路径下没有找到名为'mysqldump'的mysql工具,MysqlDumpUtil类的功能依赖此文件!");
         }
-
-        //构建远程数据库命令字符串  
+        String filePath;
+        if (isWindows()) {
+            filePath = "E:\\backup\\db\\backup." + new Date().getTime();
+        } else {
+            filePath = "/var/nupday/backup/db/backup." + new Date().getTime();
+        }
+        File saveFile = new File(filePath);
+        if (!saveFile.getParentFile().exists()) {
+            saveFile.getParentFile().mkdirs();
+        }
+        PrintWriter printWriter = null;
+        BufferedReader bufferedReader = null;
+        //构建远程数据库命令字符串
         StringBuilder cmdStr = new StringBuilder();
         cmdStr.append(" -u")
                 .append(dbConfig.getUserName())
@@ -44,56 +58,41 @@ public class DBUtil {
                 .append(" ").append(dbConfig.getDatabase());
 
         Runtime rt = Runtime.getRuntime();
-        // 调用mysqldump的cmd命令  
-        final Process p;
+        // 调用mysqldump的cmd命令
+        final Process process;
         if (isWindows()) {
-            p = rt.exec(new String[]{"cmd", "/c", f.getPath() + cmdStr.toString()});
+            process = rt.exec(new String[]{"cmd", "/c", mysqlFile.getPath() + cmdStr.toString()});
         } else {
-            p = rt.exec(new String[]{"sh", "-c", "cd " + f.getParent() + " | mysqldump " + cmdStr.toString()});
+            process = rt.exec(new String[]{"sh", "-c", "cd " + mysqlFile.getParent() + " | mysqldump " + cmdStr.toString()});
         }
-
-        //得到远程数据库备份的流文件
-        InputStream in = p.getInputStream();
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        new Thread(() -> {
-            String line1;
-            Integer count = 0;
-            try {
-                while ((line1 = reader.readLine()) != null) {
-                    if (line1 != null) {
-                        count++;
-                        stringBuilder.append(line1).append("\r\n");
-                    } else {
-                        break;
-                    }
-                }
-            } catch (Exception e) {
+        try {
+            printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(saveFile), "utf8"));
+            InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream(), "utf8");
+            bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            while((line = bufferedReader.readLine())!= null){
+                printWriter.println(line);
             }
-            logger.info(stringBuilder.toString());
-            logger.info(count + "");
-        }).start();
-        InputStream is2 = p.getErrorStream();
-        new Thread(() -> {
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(is2));
+            printWriter.flush();
+            if(process.waitFor() == 0){//0 表示线程正常终止。
+                return saveFile;
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        } finally {
             try {
-                String line2;
-                while ((line2 = br2.readLine()) != null) {
-                    if (line2 != null) {
-                    }
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (printWriter != null) {
+                    printWriter.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                try {
-                    is2.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
-        }).start();
-        p.waitFor();
-        return new ByteArrayInputStream(stringBuilder.toString().getBytes());
+        }
+        return saveFile;
+
     }
 
     /**
