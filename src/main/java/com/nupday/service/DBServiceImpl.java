75eff47f9cc2cf9a484487d345562e4bd14ca5e5
Neil Wan <neilw@synnex.org>
2018-07-24 13:49:32 +0800

    update dbsevice

--- src/main/java/com/nupday/service/DBServiceImpl.java
+++ src/main/java/com/nupday/service/DBServiceImpl.java
@@ -1,24 +1,30 @@
 package com.nupday.service;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import com.nupday.config.DBInfo;
-import com.nupday.util.DBUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBServiceImpl implements DBService{
     @Autowired
-    private DBInfo dbInfo;
-    @Autowired
     private COSService cosService;
 
-    public void backUpDB() throws IOException, InterruptedException {
-        DBUtil.backup(dbInfo);
-//        String fileName = "backup." + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-//        cosService.putObject(in, fileName, "db-backup");
+    public void backUpDB() throws FileNotFoundException {
+        String path;
+        if (System.getProperty("os.name").indexOf("Windows") != -1) {
+            path = "E:\\db_backup.sql";
+        } else {
+            path = "/var/backup/nupday/db/db_backup.sql";
+        }
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+        String fileName = "backup." + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        cosService.putObject(new FileInputStream(file), fileName, "db-backup");
     }
 }
